17364b6dd1d6c44a1f22593dfc8269ca479b25a8
DK199629 <i.darko371022@presbyuniversity.edu.gh>
2025-01-11 13:38:22 -0500

    new added changes

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// script.js
+document.querySelectorAll('.gallery-item img').forEach(image => {
+    image.addEventListener('click', () => {
+        const overlay = document.createElement('div');
+        overlay.classList.add('overlay');
+        overlay.innerHTML = `<img src="${image.src}" alt="${image.alt}" />`;
+        document.body.appendChild(overlay);
+
+        overlay.addEventListener('click', () => {
+            overlay.remove();
+        });
+    });
+});
